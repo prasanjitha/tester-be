b39edda5ab0398eb7a2f67f7295012e98260bdb5
prasanjitha <graphicdis126@gmail.com>
2022-09-30 21:38:44 +0530

    implement_user_authentication

--- src/school/models/school-entity.ts
+++ src/school/models/school-entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "src/auth/user.entity";
+import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity('school')
 export class SchoolEntity {
@@ -10,5 +11,11 @@ export class SchoolEntity {
 
     @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
     createdAt: Date;
+
+    @OneToMany(type => User, user => user.school, { eager: true })
+    user: User[];
+
+    @Column()
+    userId: number;
 
 }
--- src/school/services/school.service.ts
+++ src/school/services/school.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { from, Observable } from 'rxjs';
+import { User } from 'src/auth/user.entity';
 import { Repository } from 'typeorm';
 import { CreateSchoolDto } from '../dto/create-school.dto';
 import { SchoolEntity } from '../models/school-entity';
@@ -13,7 +14,21 @@ export class SchoolService {
         private readonly schoolRepository: Repository<SchoolEntity>
     ) { }
 
-    createSchool(createSchoolDto: CreateSchoolDto): Observable<School> {
-        return from(this.schoolRepository.save(createSchoolDto));
+    async createSchool(createSchoolDto: CreateSchoolDto, user: User): Promise<School> {
+        const { name } = createSchoolDto;
+        const school = new SchoolEntity();
+        console.log(user);
+        school.name = name;
+        school.userId = user.id;
+        return await this.schoolRepository.save(school);
+    }
+
+    getUserSchool(id: number): Observable<School[]> {
+        const query = this.schoolRepository.createQueryBuilder('school');
+        query.where('school.userId =:userId', { userId: id });
+        const tasks = from(query.getMany());
+        return tasks;
+
+
     }
 }
